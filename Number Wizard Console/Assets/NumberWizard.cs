7a1967d2321ac740911e1975b881f6c462d64609
Alexandru Noroc <kradores10@gmail.com>
2019-02-19 22:31:00 +0200

    First Commit - checking Input

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		int min = 1;
+		int max = 1000;
+		Debug.Log("Welcome to number wizard, yo");
+		Debug.Log("Please choose a number between:\n" + min + " and " + max);
+		Debug.Log("Tell me if your number is higher or lower than 500");
+		Debug.Log("Push up = Higher, Push Down = Lower, Push Enter = Correct");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Debug.Log("Up Arrow key was pressed.");
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Debug.Log("Down Arrow key was pressed.");
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("Enter key was pressed.");
+		}
+
+	}
+}
